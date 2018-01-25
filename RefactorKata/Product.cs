280366e89a9e0667b3db355fa6c8fc55b58badfb
Jack Mentzer <jackdanielmentzer@gmail.com>
2018-01-25 09:29:23 -0500

    finished refactor by using IDisposable

--- RefactorKata/Product.cs
+++ RefactorKata/Product.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RefactorKata
+﻿namespace RefactorKata
 {
     class Product
     {
-        public string Name { get; set }
+        public string Name { get; set; }
     }
 }
-
--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -1,31 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+
 namespace RefactorKata
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
-
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-            var reader = cmd.ExecuteReader();
-            var products = new List<Product>();
-
-            //TODO : Replace with Dapper
-            while (reader.Read())
+            // TODO : Add connection string to config once config is taught
+            using (var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
             {
-                var prod = new Product { Name = reader["Name"].ToString() };
-                products.Add(prod);
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+
+                var reader = cmd.ExecuteReader();
+                var products = new List<Product>();
+
+                //TODO : Replace with Dapper once Dapper is taught
+                while (reader.Read())
+                {
+                    var prod = new Product { Name = reader["Name"].ToString() };
+                    products.Add(prod);
+                }
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
             }
-            conn.Dispose();
-            foreach (var product in products)
-            {
-                Console.WriteLine(product.Name);
-            }
-                ;
         }
     }
 }
